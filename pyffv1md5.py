1ada329b39708087762c55f3365ed8f5d8a74ea7
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-10-31 12:19:49 +0000

    comments

--- pyffv1md5.py
+++ pyffv1md5.py
@@ -8,18 +8,21 @@ fmd5ffv1 = output + ".framemd5"
 print filename
 print output
 
-
+#Convert video to lossless ffv1 and generate md5 checksums for every frame
 subprocess.call(['ffmpeg','-i', filename, '-c:v', 'ffv1','-g','1','-level','3', '-c:a','copy', output, '-f','framemd5','-an', fmd5 ])
 subprocess.call(['ffmpeg','-i', output, '-f','framemd5','-an', fmd5ffv1 ])
 print fmd5
 print fmd5ffv1
 #http://bioportal.weizmann.ac.il/course/python/PyMOTW/PyMOTW/docs/filecmp/index.html and https://docs.python.org/2/library/filecmp.html
+
+#compare the two checksum files
 if filecmp.cmp(fmd5, fmd5ffv1, shallow=False): 
 	print "YOUR FILES ARE LOSSLESS YOU SHOULD BE SO HAPPY!!!"
 else:
 	print "YOUR CHECKSUMS DO NOT MATCH, BACK TO THE DRAWING BOARD!!!"
 
-#http://www.tutorialspoint.com/python/python_files_io.htm
+#Next section creates mediainfo xml output for source and output. #reference#http://www.tutorialspoint.com/python/python_files_io.htm
+
 fo = open(inputxml, "w+")
 mediaxmlinput = subprocess.check_output(['mediainfo','-f','--language=raw','--output=XML', filename ])
 fo.write(mediaxmlinput)
